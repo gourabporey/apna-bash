5636f17a59f0c74af7da37ca13bbd102aad07479
Gourab Porey <gourabporey@users.noreply.github.com>
2023-04-28 14:30:09 +0530

    Refactored by using objects for instructions and parsing the input

--- main.js
+++ main.js
@@ -1,34 +1,61 @@
 const fs = require("fs");
+const commands = fs.readFileSync(process.argv[2], "utf-8");
 
-const commands = fs.readFileSync(process.argv[2], "utf-8").split("\n");
-
-const run = function(commands) {
-  let pc = 0;
-  let pwd = process.env.PWD;
-
-  while(pc < commands.length) {
-    if(commands[pc] === "ls") {
-      let fileAndDirs = fs.readdirSync(pwd);
-      console.log(fileAndDirs.join(" "));
-    }
-
-    if(commands[pc] === "pwd") {
-      console.log(process.env.PWD);
-    }
-
-    const cdRegex = /^cd/;
-    if(cdRegex.test(commands[pc])) {
-      const dir = commands[pc].split(" ")[1];
-      const dirPath = `${pwd}/${dir}`;
-
-      if(fs.existsSync(dirPath)) {
-        console.log(dirPath);
-        pwd = dirPath;
-      }
-    }
-
-    pc++;
-  }
+const tokenize = function(commands) {
+  return commands.replace(/\n*$/, "").split("\n").map(function(token) {
+    return token.split(" ");
+  });
 };
 
-run(commands);
+const parse = function(commands) {
+  const tokens = tokenize(commands);
+
+  return tokens.reduce(function(instructions, token) {
+    if(token.length === 1) {
+      return [...instructions, {command: token[0]}];
+    }
+
+    return [...instructions, {command: token[0], argument: token[1]}];
+  }, []);
+};
+
+const ls = function({pwd}) {
+  console.log(fs.readdirSync(pwd).join(" "));
+  return {pwd};
+};
+
+const pwd = function({pwd}) {
+  console.log(pwd);
+  return {pwd};
+};
+
+const cd = function({pwd}, target) {
+  const path = `${pwd}/${target}`;
+
+  if(fs.existsSync(path)) {
+    console.log(path);
+    pwd = path;
+  } else {
+    console.error(`cd: ${target}: No such file or directory`);
+  }
+
+  return {pwd};
+};
+
+const instructions = {ls, pwd, cd};
+
+const execute = function(commands) {
+  const parsedText = parse(commands);
+
+  let environment = {
+    pwd: process.env.PWD
+  };
+
+  parsedText.forEach(function(args) {
+    environment = instructions[args.command](environment, args.argument);
+  });
+
+  return environment;
+};
+
+execute(commands);
