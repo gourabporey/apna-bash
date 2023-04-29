75e37fe17fd7266381e9f28140eab6bba152e7ac
Gourab Porey <gourabporey@users.noreply.github.com>
2023-04-29 11:41:22 +0530

    Added test cases for ls and pwd

--- apna-bash-test.js
+++ apna-bash-test.js
@@ -1,14 +1,24 @@
 const {describe, it} = require("node:test");
 const {deepStrictEqual} = require("assert");
-const {execute} = require("./main.js");
+const {execute} = require("./apna-bash.js");
 
 describe("Execute()", function() {
-  it("Should return list of files and directories when ls is being run", function() {
-    deepStrictEqual(execute("ls"), "apna-bash-test.js apna-script.ab demo-dir main.js");
+  describe("ls", function() {
+    it("Should return list of files and directories when ls is being run", function() {
+      deepStrictEqual(execute("ls"), [{ message: "apna-bash-test.js apna-bash.js apna-script.ab demo-dir instructions.js main.js", code: 0}]);
+    });
   });
 
-  it("Should give the present working directory", function() {
-    deepStrictEqual(execute("pwd"), "/Users/gourabporey/projects/js/april/apna-bash");
+  describe("pwd", function() {
+    it("Should give the present working directory", function() {
+      deepStrictEqual(execute("pwd"), [{message: "/Users/gourabporey/projects/js/april/apna-bash", code: 0}]);
+    });
+  });
+
+  describe("cd", function() {
+    it("Should give the present working directory", function() {
+      deepStrictEqual(execute("cd ."), [{environment: {pwd: ""}, outputs: [{code: 0}]}]);
+    });
   });
 });
 
--- instructions.js
+++ instructions.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 
+const isVisible = function(path) {
+  return /^[^.]/.test(path);
+};
+
 const ls = function({pwd}) {
   const contents = fs.readdirSync(pwd);
-  const isVisible = /^[^.]/;
-
-  const visibleContents = contents.filter(function(content) {
-    return isVisible.test(content);
-  }).join(" ");
+  const visibleContents = contents.filter(isVisible).join(" ");
 
   return {pwd, output: {message: visibleContents, code: 0}};
 };
@@ -15,17 +15,34 @@ const pwd = function({pwd}) {
   return {pwd, output: {message: pwd, code: 0}};
 };
 
-const cd = function({pwd}, target) {
-  const path = `${pwd}/${target}`;
+const normalizePath = function(path) {
+  const pathTokens = path.split("/");
+  const normalizePathTokens = [];
+
+  pathTokens.forEach(function(token) {
+    if(token === "..") {
+      normalizePathTokens.pop();
+    } else if(token != ".") {
+      normalizePathTokens.push(token);
+    }
+  });
+
+  return normalizePathTokens.join("/");
+};
+
+const isAbsolute = function(path) {
+  return `${path}`.startsWith("/");
+};
+
+const cdError = function(path) {
+  return `cd: ${path}: No such file or directory`;
+};
+
+const cd = function({pwd}, target, ...rest) {
+  const path = isAbsolute(target) ? target : normalizePath(`${pwd}/${target}`);
   const output = {};
 
-  if(fs.existsSync(path)) {
-    output.code = 0;
-    pwd = `${path}`;
-  } else {
-    output.message = `cd: ${target}: No such file or directory`;
-    output.code = 1;
-  }
+  [pwd, output.code, output.message] = fs.existsSync(path) ? [path, 0] : [pwd, 1, cdError(path)];
 
   return {pwd, output};
 };
