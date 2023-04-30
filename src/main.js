0041fcf525b8a05a49f01aae7ffa6e3f7f4a19a7
Gourab Porey <gourabporey@users.noreply.github.com>
2023-04-30 14:18:12 +0530

    Restructure directory by making library, source and test different directories

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ const {run} = require("./apna-bash.js");
 
 const display = function(log) {
   log.forEach(function(result) {
-    const print = result.output.code === 0 ? console.log : console.error;
+    const printStream = result.output.code === 0 ? console.log : console.error;
 
     if(result.output.message !== undefined) {
-      print(`\n${result.output.message}`);
+      printStream(`\n${result.output.message}`);
     }
   });
 };
